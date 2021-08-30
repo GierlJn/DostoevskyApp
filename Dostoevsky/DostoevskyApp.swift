06566f294a170d9f97d7fafa7fbcdcf70d208383
Julian <julian.gierl@gmx.de>
2021-08-30 21:00:31 +0200

    Initial Commit

---
+++ Dostoevsky/DostoevskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DostoevskyApp.swift
+//  Dostoevsky
+//
+//  Created by Julian Gierl on 30.08.21.
+//
+
+import SwiftUI
+
+@main
+struct DostoevskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
