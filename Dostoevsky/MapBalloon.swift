aecc00fd3a76c955257c87ec3f39f1aaaf80392e
Julian <julian.gierl@gmx.de>
2022-02-22 09:44:12 +0100

    map balloon added

---
+++ Dostoevsky/MapBalloon.swift
@@ -0,0 +1,27 @@
+//
+//  MapBalloon.swift
+//  Dostoevsky
+//
+//  Created by Julian Gierl on 22.02.22.
+//
+
+import SwiftUI
+
+struct MapBalloon: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.minY), control: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.maxY), control: CGPoint(x: rect.maxX, y: rect.minY))
+        return path
+    }
+    
+
+}
+
+struct MapBalloon_Previews: PreviewProvider {
+    static var previews: some View {
+        MapBalloon()
+            .frame(width: 200, height: 150)
+    }
+}
