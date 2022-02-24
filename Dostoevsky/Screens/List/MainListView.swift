103db1387044256ed93c052b583956f3a0cc5adb
Julian <julian.gierl@gmx.de>
2022-02-24 15:39:27 +0100

    list buttons and sections added

--- Dostoevsky/Screens/List/MainListView.swift
+++ Dostoevsky/Screens/List/MainListView.swift
@@ -12,25 +12,39 @@ import SwiftUI
 struct MainListView: View {
     
     @ObservedObject var viewModel = LocationDetailViewModel()
-
+    
     var body: some View {
         NavigationView{
-            List{
-                ForEach(viewModel.locations, id: (\.self)){ location in
-                    NavigationLink {
-                        LocationDetailPushView(viewModel: viewModel, selectedLocation: location)
-                    } label: {
-                        LocationCell(location: location)
+            VStack{
+                HStack{
+                    Spacer()
+                    Button("Sort"){
+                        print("test")
                     }
                 }
+                List{
+                    Section(header: HStack{
+                        Text("Novels")
+                        
+                    }){
+                        ForEach(viewModel.locations.filter{$0.category == 1}, id: (\.self)){ location in
+                            NavigationLink {
+                                LocationDetailPushView(viewModel: viewModel, selectedLocation: location)
+                            } label: {
+                                LocationCell(location: location)
+                            }
+                        }
+                    }
+                }
+                
             }
-        }.accentColor(.white)
+            
+            
+            .navigationBarHidden(true)
+        }
         
-        
-        
+        .accentColor(.white)
     }
-    
-    
 
 }
 
