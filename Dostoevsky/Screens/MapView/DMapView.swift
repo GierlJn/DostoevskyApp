f85bd75ec2d4df8fffd1a99764f8f3ed370bdeb0
Julian <julian.gierl@gmx.de>
2022-02-24 15:10:29 +0100

    map logo added

--- Dostoevsky/Screens/MapView/DMapView.swift
+++ Dostoevsky/Screens/MapView/DMapView.swift
@@ -13,18 +13,30 @@ struct DMapview: View{
     @ObservedObject var viewModel = LocationDetailViewModel()
 
     var body: some View{
-        Map(coordinateRegion: $viewModel.region, showsUserLocation: true, annotationItems: viewModel.locations) { location in
-            MapAnnotation(coordinate: location.location.coordinate) {
-                DAnnotation(location: location)
-                    .onTapGesture {
-                        viewModel.selectedLocation = location
-                        viewModel.setup(location: location)
-                        viewModel.isShowingDetailView = true
+        ZStack{
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true, annotationItems: viewModel.locations) { location in
+                MapAnnotation(coordinate: location.location.coordinate) {
+                    DAnnotation(location: location)
+                        .onTapGesture {
+                            viewModel.selectedLocation = location
+                            viewModel.setup(location: location)
+                            viewModel.isShowingDetailView = true
+                    }
                 }
             }
+            VStack{
+                Image("map-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                    .shadow(radius: 10)
+                Spacer()
+            }
+            
         }
+        
+        .edgesIgnoringSafeArea([.top, .leading, .trailing])
         .accentColor(.white)
-
         .sheet(isPresented: $viewModel.isShowingDetailView, onDismiss: {}){
             NavigationView{
                 LocationDetailView(viewModel: viewModel)
