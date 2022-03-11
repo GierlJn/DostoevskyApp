71da824895acf65965cb09a48e918dcaa5b190bd
Julian <julian.gierl@gmx.de>
2022-03-11 14:50:02 +0100

    added pics

---
+++ Dostoevsky/Screens/List/BookOverViewList.swift
@@ -0,0 +1,20 @@
+//
+//  BookOverViewList.swift
+//  Dostoevsky
+//
+//  Created by Julian Gierl on 11.03.22.
+//
+
+import SwiftUI
+
+struct BookOverViewList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookOverViewList_Previews: PreviewProvider {
+    static var previews: some View {
+        BookOverViewList()
+    }
+}
