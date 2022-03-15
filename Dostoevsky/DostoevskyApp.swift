173160d5bc6f94b2a13b8800466d59136c4934c2
Julian <julian.gierl@gmx.de>
2022-03-15 15:30:12 +0100

    OnboardView added

--- Dostoevsky/DostoevskyApp.swift
+++ Dostoevsky/DostoevskyApp.swift
@@ -10,16 +10,15 @@ import SwiftUI
 @main
 struct DostoevskyApp: App {
   @StateObject var appState = AppState()
-    var body: some Scene {
-        WindowGroup {
+  var body: some Scene {
+    WindowGroup {
+      Group{
+        if appState.showsOnboard{
+          OnBoardView().environmentObject(appState)
+        }else{
           AppTabView().environmentObject(appState)
-            .preferredColorScheme(.dark)
-//          if appState.showsOnboard{
-//            OnBoardView().environmentObject(appState)
-//          }else{
-//            AppTabView().environmentObject(appState)
-//          }
-          
         }
+      }.preferredColorScheme(.dark)
     }
+  }
 }
