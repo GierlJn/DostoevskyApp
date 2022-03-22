e5187ac565396ef14ce1499cadfa119674f7572e
Julian <julian.gierl@gmx.de>
2022-03-22 20:09:17 +0100

    Flag removed

--- Dostoevsky/DostoevskyApp.swift
+++ Dostoevsky/DostoevskyApp.swift
@@ -9,25 +9,23 @@ import SwiftUI
 
 
 public func isRussian() -> Bool {
-  return NSLocale.preferredLanguages[0].range(of:"ru") != nil
+    return NSLocale.preferredLanguages[0].range(of:"ru") != nil
 }
 
 @main
 struct DostoevskyApp: App {
   @StateObject var appState = AppState()
   var body: some Scene {
-    
+
     WindowGroup {
-      AppTabView().environmentObject(appState).preferredColorScheme(.dark)
-//#else
-//      Group{
-//        if appState.showsOnboard{
-//          OnBoardView().environmentObject(appState)
-//        }else{
-//          AppTabView().environmentObject(appState)
-//        }
-//      }.preferredColorScheme(.dark)
-//#endif
+      Group{
+        if appState.showsOnboard{
+          OnBoardView().environmentObject(appState)
+        }else{
+          AppTabView().environmentObject(appState)
+        }
+      }.preferredColorScheme(.dark)
+        
     }
   }
 }
